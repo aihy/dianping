f6e8e06271aae9df4490d5bd442e4453449f1b00
aihy <wzhy1234@qq.com>
2020-09-08 22:52:30 +0800

    first version bug fix

--- src/main/java/ink/wzhy/dianping/controller/MainController.java
+++ src/main/java/ink/wzhy/dianping/controller/MainController.java
@@ -3,11 +3,13 @@ package ink.wzhy.dianping.controller;
 import ink.wzhy.dianping.domain.CommentDO;
 import ink.wzhy.dianping.service.CommentsService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@RestController
 @RequestMapping("/api")
 public class MainController {
     @Resource
